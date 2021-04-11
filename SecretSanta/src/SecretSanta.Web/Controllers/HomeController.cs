3cc2b62ad92885f67ebcf1327730317930ddb5eb
Joshua Lini <cybotive@gmail.com>
2021-04-10 22:47:19 -0700

    Setup Startup.cs

--- SecretSanta/src/SecretSanta.Web/Controllers/HomeController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SecretSanta.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
