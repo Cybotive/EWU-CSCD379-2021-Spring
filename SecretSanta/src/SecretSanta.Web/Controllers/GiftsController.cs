e23b6077d67987a114edbdf29fea40cdf9610336
Joshua Lini <cybotive@gmail.com>
2021-04-12 01:45:00 -0700

    Added Gifts

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ViewModels;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GiftsController : Controller
+    {
+        static List<GiftViewModel> _Gifts = new List<GiftViewModel>()
+        {
+            new GiftViewModel { Title = "Banana Costume", Desc = "A full body costume that transforms the wearer into a banana person.", Priority = 1 },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_Gifts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(GiftViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _Gifts.Add(viewModel);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            _Gifts[id].Id = id;
+            return View(_Gifts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(GiftViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _Gifts[viewModel.Id] = viewModel;
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _Gifts.RemoveAt(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSanta.Web.ViewModels
+{
+    public class GiftViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; } = "";
+        [Required]
+        [Display(Name = "Description")]
+        public string Desc { get; set; } = "";
+        [Required]
+        [Display(Name = "Gift URL")]
+        public Uri? Url { get; set; }
+        [Display(Name = "Gift Priority")]
+        public int Priority { get; set; } = -1;
+        [Display(Name = "Gift Recipient")]
+        public UserViewModel? Recipient { get; set; }
+    }
+}
