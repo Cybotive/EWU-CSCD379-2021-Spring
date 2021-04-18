4cbe298a0dd048ea18f20a3d69b8cb0025ca2625
Joshua Lini <cybotive@gmail.com>
2021-04-18 13:31:53 -0700

    Added starting config from lectures.

--- SecretSanta/src/SecretSanta.Web/scripts/main.js
+++ SecretSanta/src/SecretSanta.Web/scripts/main.js
@@ -1,16 +1,11 @@
 import '../styles/site.css';
-
 import 'alpinejs';
-
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
 library.add(fas, far, fab);
 dom.watch();
-
-
 export function setupNav() {
     return {
         toggleMenu() {
@@ -18,20 +13,21 @@ export function setupNav() {
             if (headerNav) {
                 if (headerNav.classList.contains("hidden")) {
                     headerNav.classList.remove("hidden");
-                } else {
+                }
+                else {
                     headerNav.classList.add("hidden");
                 }
             }
         }
-    }
+    };
 }
-
 export function toggleMenuWithJs() {
     var headerNav = document.getElementById("headerNav");
     if (headerNav) {
         if (headerNav.classList.contains("hidden")) {
             headerNav.classList.remove("hidden");
-        } else {
+        }
+        else {
             headerNav.classList.add("hidden");
         }
     }
