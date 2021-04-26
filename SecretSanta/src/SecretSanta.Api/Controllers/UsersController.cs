da2464a8ca31e513f4973da5558d14dae7f48d93
Joshua Lini <cybotive@gmail.com>
2021-04-25 20:59:24 -0700

    Added controller and mock data

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // /api/users
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return DataDeleteMe.Users;
+        }
+
+        // /api/users/<index>
+        [HttpGet("{index}")]
+        public string Get(int index)
+        {
+            return DataDeleteMe.Users[index];
+        }
+
+        //DELETE /api/users/<index>
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            DataDeleteMe.Users.RemoveAt(index);
+        }
+
+        // POST /api/users
+        [HttpPost]
+        public void Post([FromBody] string eventName)
+        {
+            DataDeleteMe.Users.Add(eventName);
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Api/DataDeleteMe.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace SecretSanta.Api
+{
+    public static class DataDeleteMe
+    {
+        public static List<string> Users { get; } = new()
+        {
+            "Test User",
+            "User Two",
+        };
+    }
+}
