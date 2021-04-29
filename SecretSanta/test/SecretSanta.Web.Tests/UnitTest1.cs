ba95746e71567963d9ae6b22e0e014738cfd7b19
Joshua Lini <cybotive@gmail.com>
2021-04-29 14:31:21 -0700

    Setup Web proj test configurations

---
+++ SecretSanta/test/SecretSanta.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace SecretSanta.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
