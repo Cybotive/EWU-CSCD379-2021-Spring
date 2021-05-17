fd48bed5a5b74fe5b8f20ca9e457269adcec4e8d
Joshua Lini <cybotive@gmail.com>
2021-05-16 21:34:45 -0700

    Passing homepage E2E test. Used XPath selector and Firefox.

--- SecretSanta/test/SecretSanta.E2ETests/EndToEndTests.cs
+++ SecretSanta/test/SecretSanta.E2ETests/EndToEndTests.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlaywrightSharp;
 
 namespace SecretSanta.E2ETests
 {
@@ -8,7 +16,19 @@ namespace SecretSanta.E2ETests
         [TestMethod]
         public async Task LandOnHomepage()
         {
-            
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Firefox.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("https://localhost:5001");
+
+            Assert.IsTrue(response.Ok);
+
+            var headerContent = await page.GetTextContentAsync("//html/body/header/div/a");
+            Assert.AreEqual("Secret Santa", headerContent);
         }
     }
 }
