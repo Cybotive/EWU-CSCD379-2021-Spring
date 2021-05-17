6bcd2e7480fea010a314e9752d32744cc85f9c8c
Joshua Lini <cybotive@gmail.com>
2021-05-16 21:08:01 -0700

    Setup E2E test project

---
+++ SecretSanta/test/SecretSanta.E2ETests/EndToEndTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.E2ETests
+{
+    [TestClass]
+    public class EndToEndTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
