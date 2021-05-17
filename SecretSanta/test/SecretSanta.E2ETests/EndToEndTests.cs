40e07cfa9560169cfd0ab01632ba83429663c543
Joshua Lini <cybotive@gmail.com>
2021-05-16 21:18:58 -0700

    Added ETE test proj to sln file

--- SecretSanta/test/SecretSanta.E2ETests/EndToEndTests.cs
+++ SecretSanta/test/SecretSanta.E2ETests/EndToEndTests.cs
@@ -6,7 +6,7 @@ namespace SecretSanta.E2ETests
     public class EndToEndTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task LandOnHomepage()
         {
             
         }
