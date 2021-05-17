e90d3aa87e5f622f2682df3f2dcd2cda365a9963
Joshua Lini <cybotive@gmail.com>
2021-05-17 02:04:50 -0700

    Fixed api port issue. Was still using old client in Startup.cs

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -22,15 +22,11 @@ namespace SecretSanta.Web
             };
         }
         
-        private static HttpClient UsersHttpClient { get; } = new()
-        {
-            BaseAddress = new Uri("https://localhost:5101/")
-        };
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IUsersClient, UsersClient>(_ => new UsersClient(UsersHttpClient));
+            services.AddScoped<IUsersClient, UsersClient>(_ => new UsersClient(ApiClient));
             services.AddControllersWithViews();
         }
 
