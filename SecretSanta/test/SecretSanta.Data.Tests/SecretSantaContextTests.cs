4b509961b08451327478333a5e2d82c43b78d5ec
Joshua Lini <cybotive@gmail.com>
2021-06-06 12:16:31 -0700

    + Tests class for database

---
+++ SecretSanta/test/SecretSanta.Data.Tests/SecretSantaContextTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.Data.Tests
+{
+    [TestClass]
+    public class SecretSantaContextTests
+    {
+        [TestMethod]
+        public void UsersDbSet_AddingValidUser_IncrementsCount()
+        {
+            
+        }
+    }
+}
