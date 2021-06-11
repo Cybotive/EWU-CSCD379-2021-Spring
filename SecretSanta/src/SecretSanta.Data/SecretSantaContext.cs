11df589fffccfa594e8aae9a2c57ea5056b17b42
Joshua Lini <cybotive@gmail.com>
2021-06-11 04:13:00 -0700

    Removed modelBuilders from context

--- SecretSanta/src/SecretSanta.Data/SecretSantaContext.cs
+++ SecretSanta/src/SecretSanta.Data/SecretSantaContext.cs
@@ -30,38 +30,6 @@ namespace SecretSanta.Data
             {
                 throw new ArgumentNullException(nameof(modelBuilder));
             }
-
-            /*modelBuilder.Entity<User>()
-                //.UsePropertyAccessMode(PropertyAccessMode.Property)
-                .HasKey(user => user.Id);
-            //modelBuilder.Entity<User>()
-                //.HasAlternateKey(user => new { user.FirstName, user.LastName });
-
-            modelBuilder.Entity<Gift>()
-                .HasKey(gift => gift.Id);
-            //modelBuilder.Entity<Gift>()
-                //.HasAlternateKey(gift => new { gift.Title });
-
-            modelBuilder.Entity<Group>()
-                //.UsePropertyAccessMode(PropertyAccessMode.Property)
-                .HasKey(group => group.Id);
-            //modelBuilder.Entity<Group>()  
-                //.HasAlternateKey(group => new { group.Name });
-
-            modelBuilder.Entity<Assignment>()
-                .HasKey(assign => assign.Id);*/
-
-            modelBuilder.Entity<Group>()
-                .HasMany(group => group.Users)
-                .WithMany(group => group.Groups);
-
-            modelBuilder.Entity<Group>()
-                .HasMany(group => group.Assignments)
-                .WithOne(assign => assign.Group);
-
-            modelBuilder.Entity<User>()
-                .HasMany(user => user.Gifts)
-                .WithOne(user => user.Receiver);
         }
     }
 }
