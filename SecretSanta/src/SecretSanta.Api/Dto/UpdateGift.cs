d3fe9e49ee49de647c86940572e137516d10901d
Joshua Lini <cybotive@gmail.com>
2021-06-11 19:51:34 -0700

    Added more gift DTOs

---
+++ SecretSanta/src/SecretSanta.Api/Dto/UpdateGift.cs
@@ -0,0 +1,11 @@
+﻿namespace SecretSanta.Api.Dto
+{
+    public class UpdateGift
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public int? Priority { get; set; }
+        public User? Receiver { get; set; }
+    }
+}
